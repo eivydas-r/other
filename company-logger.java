a5401fc438ee33b0417da3f23c572b3d8d6efd98
eivydas <36181330+eivydas-r@users.noreply.github.com>
2019-08-02 14:57:56 -0500

    main structure begun, inplemented error exceptions

--- company-logger.java
+++ company-logger.java
@@ -17,8 +17,35 @@ Successfully logged [FULL NAME] out at 0:00:00
 
 */
 
-public class HelloWorld{
+import java.util.Scanner;
+
+public class Logger{
      public static void main(String []args){
-        System.out.println("Hello World");
+         try{
+             intro();
+         } catch (Exception e){
+             System.out.println("\n[ERROR] Error caught:\n\t"+e);
+         }
+     }
+     
+     public static void intro(){
+         System.out.println("COMPANY LOGIN: \n\tEmployee: [e]\n\tVisitor/Guest: [v/g]\n\nType login type [e/v/g] for options: ");
+         
+        Scanner scan = new Scanner(System.in);
+        
+        if(scan.hasNextLine()){
+            String input = scan.nextLine();
+        } else { 
+            throw new IllegalArgumentException("\n\tNo Scanner input available in current terminal.");
+        }
+
+     }
+     
+     public static void login(){
+         
+     }
+     
+     public static void logout(){
+         
      }
 }

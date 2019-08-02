646bc87d1f68774e036838101c5133cca82f3842
eivydas <36181330+eivydas-r@users.noreply.github.com>
2019-08-02 14:24:14 -0500

    Create company-logger.java

---
+++ company-logger.java
@@ -0,0 +1,24 @@
+// employee/visitor terminal logger
+/*
+COMPANY LOGIN:
+    Employee: [e]
+    Visitor/Guest: [v/g]
+
+Type login type [e/v/g] for options: 
+
+Employee first name:
+Employee last name:
+
+Successfully logged [FULL NAME] in at 0:00:00
+
+// if already logged in, automatically log out
+
+Successfully logged [FULL NAME] out at 0:00:00
+
+*/
+
+public class HelloWorld{
+     public static void main(String []args){
+        System.out.println("Hello World");
+     }
+}

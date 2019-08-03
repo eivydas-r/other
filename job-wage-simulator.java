1b44b3602154362224cab8e70ebf74ffe0abb4fa
eivydas <36181330+eivydas-r@users.noreply.github.com>
2019-08-02 19:41:12 -0500

    Create job-wage-simulator.java

---
+++ job-wage-simulator.java
@@ -0,0 +1,31 @@
+// job/wage simulator
+// by Eivydas Raulynaitis
+/*
+Job Simulator
+    Enter human name:
+    Enter hourly wage:
+    Enter tax rate: 26%
+    Enter hours worked per week (>40 = 1.5x OT):
+    Enter simulation time (in weeks):
+    
+
+*/
+
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String []args){
+        System.out.println("Enter human name: ")
+        System.out.println("Enter tax rate: ")"
+        System.out.println("Enter hours worked per week (>40 = 1.5x OT): ")
+        System.out.println("Enter simulation time (in weeks):")
+        
+    }
+    
+    // simplify sleep()
+    public static void sleep(int time){
+        try{
+            Thread.sleep(time);
+        } catch (Exception e) {}
+    }
+}

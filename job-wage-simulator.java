a7a7aad5a7ad07302816c6c04254deacd0c9d2a0
eivydas <36181330+eivydas-r@users.noreply.github.com>
2019-08-02 20:11:55 -0500

    workin

--- job-wage-simulator.java
+++ job-wage-simulator.java
@@ -1,5 +1,4 @@
 // job/wage simulator
-// by Eivydas Raulynaitis
 /*
 Job Simulator
     Enter human name:
@@ -15,11 +14,25 @@ import java.util.Scanner;
 
 public class Main{
     public static void main(String []args){
-        System.out.println("Enter human name: ")
-        System.out.println("Enter tax rate: ")"
-        System.out.println("Enter hours worked per week (>40 = 1.5x OT): ")
-        System.out.println("Enter simulation time (in weeks):")
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter human name: ");
+        String name = scan.nextLine();
+        System.out.println("Enter hourly wage:");
+        float wage = scan.nextFloat();
+        System.out.println("Enter tax rate: ");
+        float tax = scan.nextFloat();
+        System.out.println("Enter hours worked per week (>40 = 1.5x OT): ");
+        int hours = scan.nextInt();
+        System.out.println("Enter simulation time (in weeks):");
+        int weeks = scan.nextInt();
+        float total = 0;
         
+        for(int i = 1; i<=weeks; i++){
+            total = total + wage*hours;
+            System.out.format("%-" + 10 + "s   %-10s %4s\n", "Week "+i, "Wage: "+total, "Net wage: "+(total-(total*(tax/100))));
+            //System.out.println("Week "+i+": "+total);
+            sleep(1000);
+        }
     }
     
     // simplify sleep()
